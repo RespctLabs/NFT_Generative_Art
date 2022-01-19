792a04b4c854971307717c17632aee43ecf9d24a
shubham-sonthalia <shubham18366@iiitd.ac.in>
2022-01-19 19:23:55 +0530

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-const myArgs = process.argv.slice(2);
-const { buildSetup, createFiles, createMetaData } = require("./src/main.js");
-const { defaultEdition } = require("./src/config.js");
-const edition = myArgs.length > 0 ? Number(myArgs[0]) : defaultEdition;
+const basePath = process.cwd();
+const { startCreating, buildSetup } = require(`${basePath}/src/main.js`);
 
 (() => {
   buildSetup();
-  createFiles(edition);
-  createMetaData();
+  startCreating();
 })();
-
